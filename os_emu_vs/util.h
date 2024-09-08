65a2b45823679e1122c061449948d963c323bc9f
briangabini <briangabini@gmail.com>
2024-09-08 16:13:22 +0800

    feat: add support for linux

--- os_emu_vs/util.h
+++ os_emu_vs/util.h
@@ -1,48 +1,53 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-#include <string>
 #include <array>
 #include <cstdlib>
 #include <iostream>
-#include <algorithm>
+
+#define windows // change based on your OS
 
 namespace Util {
-    inline constexpr char asciiHeader[] =
-        "  ______     _______.  ______   .______    _______     _______.____    ____ \n"
-        " /      |   /       | /  __  \\  |   _  \\  |   ____|   /       |\\   \\  /   / \n"
-        "|  ,----'  |   (----`|  |  |  | |  |_)  | |  |__     |   (----` \\   \\/   /  \n"
-        "|  |        \\   \\    |  |  |  | |   ___/  |   __|     \\   \\      \\_    _/   \n"
-        "|  `----.----)   |   |  `--'  | |  |      |  |____.----)   |       |  |     \n"
-        " \\______|_______/     \\______/  | _|      |_______|_______/        |__|     \n"
-        "                                                                            \n";
+	inline constexpr char asciiHeader[] =
+		"  ______     _______.  ______   .______    _______     _______.____    ____ \n"
+		" /      |   /       | /  __  \\  |   _  \\  |   ____|   /       |\\   \\  /   / \n"
+		"|  ,----'  |   (----`|  |  |  | |  |_)  | |  |__     |   (----` \\   \\/   /  \n"
+		"|  |        \\   \\    |  |  |  | |   ___/  |   __|     \\   \\      \\_    _/   \n"
+		"|  `----.----)   |   |  `--'  | |  |      |  |____.----)   |       |  |     \n"
+		" \\______|_______/     \\______/  | _|      |_______|_______/        |__|     \n"
+		"                                                                            \n";
 
-    inline constexpr int commandsCount = 7;
+	inline constexpr int commandsCount = 7;
 
-    inline constexpr std::array<const char*, commandsCount> commands = {
-        "initialize",
-        "screen",
-        "scheduler-test",
-        "scheduler-stop",
-        "report-util",
-        "clear",
-        "exit"
-    };
+	inline constexpr std::array<const char*, commandsCount> commands = {
+		"initialize",
+		"screen",
+		"scheduler-test",
+		"scheduler-stop",
+		"report-util",
+		"clear",
+		"exit"
+	};
 
-    inline bool checkIfCommandExists(const std::string_view command) {
-        return std::ranges::find_if(commands, [&command](const char* cmd) {
-            return command == cmd;
-            }) != commands.end();
-    }
+	inline bool checkIfCommandExists(const std::string_view command) {
+		return std::ranges::find_if(commands, [&command](const char* cmd) {
+			return command == cmd;
+			}) != commands.end();
+	}
 
-    inline void flushScreen() {
-        std::cout << std::flush;
-    }
+	inline void flushScreen() {
+		std::cout << std::flush;
+	}
 
-    inline void clearScreen() {
-        flushScreen();
-        system("cls");
-    }
+	inline void clearScreen() {
+		flushScreen();
+
+	#ifdef windows
+		std::system("cls");
+	#else
+		std::system("clear");
+	#endif
+	}
 }
 
 #endif //UTIL_H
