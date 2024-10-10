b65bb7c9d8abba9a28ea460fca265a5a497f1635
CarlMatthewVerano <carlmatthewverano@gmail.com>
2024-10-10 23:26:24 +0800

    feat: functionality to update process state to finish

--- os_emu_vs/SchedulerWorker.cpp
+++ os_emu_vs/SchedulerWorker.cpp
@@ -1,34 +1,37 @@
-#include "SchedulerWorker.h"
 #include "GlobalScheduler.h"
+#include "SchedulerWorker.h"
 #include <iostream>
 
 void SchedulerWorker::update(bool isRunning) {
-    this->running = isRunning;
+	this->running = isRunning;
 }
 
 void SchedulerWorker::assignProcess(std::shared_ptr<Process> process) {
-    this->currentProcess = process;
-    this->update(true);
+	this->currentProcess = process;
+	this->update(true);
 }
 
 void SchedulerWorker::run() {
-    while (true) {
-        if (currentProcess) {
-            currentProcess->setState(currentProcess->RUNNING);
-            currentProcess->setCpuCoreId(cpuCoreId);
+	while (true) {
+		if (currentProcess) {
+			currentProcess->setState(currentProcess->RUNNING);
+			currentProcess->setCpuCoreId(cpuCoreId);
 
-            while (!currentProcess->isFinished()) {
-                currentProcess->executeCurrentCommand();
-                currentProcess->moveToNextLine();
-                sleep(250);
-            }
-            currentProcess = nullptr;
-            running = false;
-        }
-    }
+			while (!currentProcess->isFinished()) {
+				currentProcess->executeCurrentCommand();
+				if (currentProcess->getCommandCounter() == currentProcess->getLinesOfCode() - 1) {
+					currentProcess->setState(currentProcess->FINISHED);
+				}
+				currentProcess->moveToNextLine();
+				sleep(250);
+			}
+			currentProcess = nullptr;
+			running = false;
+		}
+	}
 }
 
 
 bool SchedulerWorker::isRunning() {
-    return running;
+	return running;
 }
