249d2458ed84664b350f92fe437f56a05bb44847
briangabini <briangabini@gmail.com>
2024-10-10 23:52:43 +0800

    feat: update IETThread

--- os_emu_vs/IETThread.cpp
+++ os_emu_vs/IETThread.cpp
@@ -2,11 +2,22 @@
 
 #include "IETThread.h"
 
+IETThread::IETThread() : running(false) {}
 
-void IETThread::start() {
-	std::thread(&IETThread::run, this).detach();
+IETThread::~IETThread()
+{
+	if (thread.joinable())
+	{
+		running = false;
+		thread.join();
+	}
 }
 
-void IETThread::sleep(int ms) {
-	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+void IETThread::start() {
+	running = true;
+	thread = std::thread(&IETThread::run, this);
+}
+
+void IETThread::sleep(int timeInMillis) {
+	std::this_thread::sleep_for(std::chrono::milliseconds(timeInMillis));
 }
